666781c01746d98e0ec4b91f1763d283f71b1809
Mohammad Bhuyia <126366193+Riyan096@users.noreply.github.com>
2024-12-02 16:55:55 -0500

    Time: 0 ms (100.00%) | Memory: 11.8 MB (6.47%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.cpp
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     vector<string> fizzBuzz(int n) {
         vector<string> answer;
-        answer.reserve(n);
-
-        for (int i = 1; i <= n; i++) {
-            string result;
-            if (i % 3 == 0){
-                result += "Fizz";
+        for(int i = 1; i <= n; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                answer.push_back("FizzBuzz");
             }
-            if (i % 5 == 0) {
-                result += "Buzz";
+            else if(i % 3 == 0){
+                answer.push_back("Fizz");
             }
-            if (result.empty()) {
-                result = to_string(i);
+            else if(i % 5 == 0){
+                answer.push_back("Buzz");
             }
-            answer.push_back(result);
+            else{
+                answer.push_back(to_string(i));
+            }
         }
-
         return answer;
     }
 };
