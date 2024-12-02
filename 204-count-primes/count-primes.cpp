725bb4b7beeaf8ea83e09b0e58443d5eb449f5af
Mohammad Bhuyia <126366193+Riyan096@users.noreply.github.com>
2024-12-01 22:27:37 -0500

    Time: 85 ms (98.48%) | Memory: 10.8 MB (99.24%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -4,9 +4,10 @@ public:
         if (n <= 2) {
             return 0;
         }
-
-        int count = 1;
+        
         vector<bool> prime((n + 1) / 2, true);
+        prime[0] = false;
+
         for (int i = 3; i * i < n; i += 2) {
             if (prime[i / 2]) {
                 for (int j = i * i; j < n; j += 2 * i) {
@@ -14,11 +15,15 @@ public:
                 }
             }
         }
+
+        int count = 1;
         for (int i = 3; i < n; i += 2) {
             if (prime[i / 2]) {
                 count++;
             }
         }
+
         return count;
     }
+
 };
