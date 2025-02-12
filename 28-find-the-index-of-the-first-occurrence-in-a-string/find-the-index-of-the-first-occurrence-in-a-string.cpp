b0dd7354f4bda468a521b5d45819eadffa472817
Mohammad Bhuyia <126366193+Riyan096@users.noreply.github.com>
2025-02-12 15:11:39 -0500

    Time: 0 ms (100.00%) | Memory: 8.6 MB (58.16%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-    vector<int> preprocess(const string pattern) {
-        vector<int> badChar(256, -1);
-        for (int i = 0; i < pattern.size(); i++) 
-            badChar[pattern[i]] = i;
-        return badChar;
-    }
     int strStr(string haystack, string needle) {
-        int n = haystack.size(), m = needle.size();
-        if (m > n) return -1;
+        int i = 0, j = 0;
 
-        vector<int> badChar = preprocess(needle);
-        int index = 0;
-
-        while (index <= (n - m)) {
-            int j = m - 1;
-
-            while (j >= 0 && needle[j] == haystack[index + j])
-                j--;
-
-            if (j < 0) 
-                return index;
-
-            index += max(1, j - badChar[haystack[index + j]]);
+        while (i <= haystack.length()) {
+            if (haystack[i] == needle[j]) {
+                i++;
+                j++;
+                if (j == needle.length()) {
+                    return i - j;
+                }
+            } else {
+                i = i - j + 1;
+                j = 0;
+            }
         }
         return -1;
     }
