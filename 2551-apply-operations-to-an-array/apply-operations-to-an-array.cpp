c216a30716086dcdbf7e0021bd6bbaf913503987
Mohammad Bhuyia <126366193+Riyan096@users.noreply.github.com>
2025-02-28 23:18:20 -0500

    Time: 0 ms (100.00%) | Memory: 13.3 MB (23.58%) - LeetSync

---
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        if(nums.size() > 2){
+            for(int i = 0; i < nums.size() - 1; i++){
+                if(nums[i] == nums[i + 1]){
+                    nums[i] *= 2;
+                    nums[i + 1] = 0;
+                }
+            }
+        }
+        vector<int> nonZero, zeros;
+        for (int num : nums) {
+            if (num != 0) {
+                nonZero.push_back(num);
+            } else {
+                zeros.push_back(num);
+            }
+        }
+        nums = nonZero;
+        nums.insert(nums.end(), zeros.begin(), zeros.end());
+        return nums;
+    }
+    
+};
