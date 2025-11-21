f39de82532c6b1849cc24004cdf69c3b50fe3474
Mohammad Bhuyia <126366193+Riyan096@users.noreply.github.com>
2025-11-21 17:04:15 -0500

    Time: 0 ms (100.00%) | Memory: 8.4 MB (85.19%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int result  = 0;
+        int bit = 1 << 30;
+        while (bit > x) bit >>= 2;
+        while (bit != 0) {
+            if (x >= result + bit) {
+                x -= result + bit;
+                result = (result >> 1) + bit;
+            } else {
+                result >>= 1;
+            }
+            bit >>= 2;
+        }
+        return result;
+
+    }
+};
